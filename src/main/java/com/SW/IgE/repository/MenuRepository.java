c8028d5eceed2eea1921acc532bae730f562a600
parkjto <soomj99@gmail.com>
2024-11-10 21:06:29 +0900

    24.11.10 commit

---
+++ src/main/java/com/SW/IgE/repository/MenuRepository.java
@@ -0,0 +1,8 @@
+package com.SW.IgE.repository;
+
+import com.SW.IgE.entity.Menu;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuRepository extends JpaRepository<Menu, Integer> {
+}
